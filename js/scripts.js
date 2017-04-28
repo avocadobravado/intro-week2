a031dd67a0d2a51079c849beb1fd83a3956cefb9
Rose Sponseller <rose@Roses-MacBook-Pro.local>
2017-04-28 11:18:28 -0700

    Add IDs in HTML for form inputs and declare variables in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+    var companySize = parseInt($("#companySize").val());
+    var productType = parseInt($("#productType").val());
+    var gameDev = parseInt($("#gameDev").val());
+    var uxDesign = parseInt($("#uxDesign").val());
+    var crossPlatform = parseInt($("#crossPlatform").val());
+
     event.preventDefault();
-    $("#initial-showing").hide();
-    $("#ruby").fadeIn(1000);
   });
 });
+
+
+//var results =
+
+// $("#initial-showing").hide();
+// $("#ruby").fadeIn(1000);
+// $("#design").fadeIn(1000);
