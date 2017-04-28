b7843eddbf4cc2f87755be543507d61198b8635a
Rose Sponseller <rose@Roses-MacBook-Pro.local>
2017-04-28 16:38:29 -0700

    Add spans for name display in index.html, then set variable for userName in js file and ensure each result displays name, and add darker h3 stylings just for name in css file

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ var add = function(num1, num2, num3, num4, num5) {
 // Front end logic
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+    var userName = $("input#userName").val();
     var companySize = parseInt($("#companySize").val());
     var productType = parseInt($("#productType").val());
     var gameDev = parseInt($("#gameDev").val());
@@ -19,22 +20,22 @@ $(document).ready(function() {
     if (userInput <= 10) {
       $("#initial-showing").hide();
       $("#frontEnd").fadeIn(1000);
-      console.log(userInput);
+      $(".userName").text(userName);
     }
     else if (userInput === 11 || userInput <= 13) {
       $("#initial-showing").hide();
       $("#mobileDev").fadeIn(1000);
-      console.log(userInput);
+      $(".userName").text(userName);
     }
     else if (userInput === 14 || userInput <= 16) {
       $("#initial-showing").hide();
       $("#gameDevelopment").fadeIn(1000);
-      console.log(userInput);
+      $(".userName").text(userName);
     }
     else {
       $("#initial-showing").hide();
       $("#backEnd").fadeIn(1000);
-      console.log(userInput);
+      $(".userName").text(userName);
     }
   });
 });
