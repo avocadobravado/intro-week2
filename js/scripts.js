3075f8f942927fe3be3477c3fde0224edd87655f
Rose Sponseller <rose@Roses-MacBook-Pro.local>
2017-04-28 11:54:40 -0700

    Add initial logic to js file that is starting to work...

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Business logic
+var add = function(num1, num2, num3, num4, num5) {
+  return num1 + num2+ num3 + num4 + num5;
+};
+
+// Front end logic
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     var companySize = parseInt($("#companySize").val());
@@ -5,8 +11,19 @@ $(document).ready(function() {
     var gameDev = parseInt($("#gameDev").val());
     var uxDesign = parseInt($("#uxDesign").val());
     var crossPlatform = parseInt($("#crossPlatform").val());
+    var userInput = add(companySize, productType, gameDev, uxDesign, crossPlatform);
 
+// Prevent default to display results directly on page
     event.preventDefault();
+
+    if (userInput < 14) {
+      $("#initial-showing").hide();
+      $("#ruby").fadeIn(1000);
+      $("#design").fadeIn(1000);
+      $("#php").fadeIn(1000);
+    }
+    // else (userInput < 20) {
+    //   $("#c").show();
   });
 });
 
